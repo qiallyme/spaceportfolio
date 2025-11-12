fe91553c153b462c918e94f33978f6dc4c3ffbcc
qiallyme <crice4485@gmail.com>
2025-11-12 02:13:14 -0500

    init

---
+++ config/index.ts
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Cody Rice-Velasquez | Systems Whisperer & Business Operations Consultant",
+  description: "I'm Cody Rice-Velasquez - systems whisperer, chaos-to-clarity alchemist, and the brain behind QiAlly & EmpowerQNow. From Evansville roots to reviving broken empires, I bridge the gap between disarray and operations. I've delivered $3M+ in savings and optimized life easier.",
+  keywords: [
+    "Cody Rice-Velasquez",
+    "Business Consultant",
+    "Operations Directory",
+    "Executive Strategist",
+    "Software Engineer",
+    "Business Refiner",
+    "Novis Developer",
+    "react",
+    "reactjs",
+    "node",
+    "nodejs",
+    "sveltekit",
+    "tailwindcss",
+    "vercel",
+    "python",
+    "nextjs",
+    "space-portfolio",
+    "portfolio",
+    "react-icons",
+    "framer-motion",
+    "typescript",
+    "javascript",
+  ] as Array<string>,
+  authors: {
+    name: "Cody Rice-Velasquez",
+    url: "https://portfolio.qially.com",
+  },
+} as const;
